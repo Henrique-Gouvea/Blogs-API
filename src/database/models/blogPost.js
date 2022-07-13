d6379adc807a36cc944a119b9fe2225074e51f20
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-07-13 18:24:27 -0300

    feat(models): create blog post in models

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,24 @@
+'use strict';
+const createBlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {
+    underscored: true,
+    tableName: 'BlogPosts'
+  })
+
+  BlogPost.associate = (models) => {
+    PostCategory.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'userId'
+    })
+  }
+
+  return BlogPost;
+};
+
+module.exports = createBlogPostModel;
