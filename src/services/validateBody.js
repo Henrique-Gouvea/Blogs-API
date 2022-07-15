e07936025db54bd5d8138b53cbd39570d3b4c28a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-07-15 18:10:58 -0300

    fix(models): Change name of models

--- src/services/validateBody.js
+++ src/services/validateBody.js
@@ -1,4 +1,22 @@
 // const loginSchema = require('../schema/loginSchema');
+const { User } = require('../database/models');
+const { checkPassword } = require('./password');
+
+const validateCredentials = async ({ email, password }) => {
+  const user = await User.findOne({ where: { email } });
+  console.log('2');
+  console.log(user);
+  if (!user) {
+    const e = new Error('Invalid fields');
+    e.name = 'ValidationError';
+    throw e;
+  }
+  console.log(password);
+  console.log(user.password);
+
+  // checkPassword(password, user.password);
+  return '123456';
+};
 
 const validateBody = ({ email, password }) => {
   if (!email || !password) {
@@ -9,4 +27,4 @@ const validateBody = ({ email, password }) => {
   return { email, password };
 };
 
-module.exports = validateBody;
+module.exports = { validateBody, validateCredentials };
